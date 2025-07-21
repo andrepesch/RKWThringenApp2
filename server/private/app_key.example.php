b67445687901279f8f5769ec4535270fa008be23
Pesch <apollopro@aol.com>
2025-07-21 22:38:56 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/rkwthringenapp/MainActivity.kt
    #	app/src/main/java/com/example/rkwthringenapp/ui/AppNavigation.kt
    #	app/src/main/java/com/example/rkwthringenapp/ui/AuthViewModel.kt
    #	app/src/main/java/com/example/rkwthringenapp/ui/DashboardScreen.kt
    #	app/src/main/java/com/example/rkwthringenapp/ui/LoginScreen.kt
    #	app/src/main/java/com/example/rkwthringenapp/ui/RegisterScreen.kt

---
+++ server/private/app_key.example.php
@@ -0,0 +1,5 @@
+<?php
+// app_key.php - SICHER AUSSERHALB DES WEB-ROOTS SPEICHERN!
+
+// Bewahre diesen Schlüssel absolut geheim. Er wird zur Ver- und Entschlüsselung verwendet.
+return 'IHRE_DATEN_HIER';
---
+++ server/private/db_config.example.php
@@ -0,0 +1,11 @@
+<?php
+// db_config.php - Generiert am 2025-07-21 20:22:07
+return [
+    'host' => 'IHRE_DATEN_HIER',
+    'dbname' => 'IHRE_DATEN_HIER',
+    'user' => 'IHRE_DATEN_HIER',
+    'pass_encrypted' => 'base64:OA377N4pqA1uKQnywnOc',
+    'cipher' => 'aes-256-gcm',
+    'iv' => 'base64:MFsuqaGoQOBNJX+u',
+    'tag' => 'base64:H6lcN4ZMocElcftxRlzGYQ=='
+];
